bee25433a86c8e53baafc452d2dc5f6e6e0db418
shubhamr238 <shubhamr238@gmail.com>
2020-04-11 15:02:31 +0530

    add action creator fun

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,17 @@
-{
-    type: 'ADD_MOVIES'
-
-}
+// {
+//     type: 'ADD_MOVIES'
+// }
 // {
 //     type: ''
 // }
+
+//action type vars
+export const ADD_MOVIES = 'ADD_MOVIES';
+
+//action creator func.
+export function addMovies(movies){
+    return{
+        type: ADD_MOVIES,
+        movies:movies
+    }
+}
